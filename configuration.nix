c1862187329ca531dfe7195293d23aad6fe690df
Viktor Lin <asfgjk123123@gmail.com>
2019-07-12 15:29:34 +0800

    add configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,146 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    alsa-firmware
+    alsaUtils
+    pulseaudio
+    apulse
+    inxi
+    wget
+    vim
+    neovim
+    gnome3.gdm
+    gnome3.gnome-shell
+    fcitx
+    fcitx-configtool
+    fcitx-engines.rime
+    librime
+    firefox-devedition-bin
+    chromium
+    chrome-gnome-shell
+    git
+    file
+    noto-fonts-cjk
+    zsh
+    fira
+    fira-code
+    fira-code-symbols
+    wqy_microhei
+    tmux
+    (python2.withPackages(ps: with ps; [ pygtk pynvim ]))
+    (python3.withPackages(ps: with ps; [ requests pynvim ]))
+    gcc
+    gnumake
+    lsof
+    busybox
+    ghc
+    (all-hies.selection { selector = p: { inherit (p) ghc864; }; })
+    python27Packages.pip
+    python37Packages.pip
+    pkg-config
+    gnome3.nautilus
+    gnome3.nautilus-python
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  i18n.inputMethod.enabled = "fcitx";
+  i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ rime ];
+
+  services.gnome3.chrome-gnome-shell.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    ohMyZsh.enable = true;
+    ohMyZsh.plugins = [ "git" ];
+    ohMyZsh.theme = "cloud";
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  fonts.fonts = with pkgs; [ noto-fonts-cjk fira fira-code wqy_microhei ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vik = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+
+}
